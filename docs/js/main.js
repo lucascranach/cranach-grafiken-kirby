f02f8bbc48945a30e9abb25d6c237d3dcd614238
cnoss <christian.noss@fh-koeln.de>
2019-06-24 17:10:34 +0200

    Aufgeräumt.

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,7 +12,7 @@ var zoomable = document.querySelector('.js-zoomable')
 if (zoomable) {
   let imgUrl = document.querySelector('img').src.replace(/.*imageserver/, '/img/graphics')
   imgUrl = imgUrl.replace(/-l\./, '-xl.')
-  zoomable.classList.add('is-faded')
+  zoomable.querySelector('img').classList.add('is-faded')
   var viewer = OpenSeadragon({
     id: zoomable.id,
     prefixUrl: '/libs/openseadragon/images/',
